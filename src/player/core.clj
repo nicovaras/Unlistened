0f345f8d2f7d4d24688ab572fdc93eb59cdb94b3
Nico Varas <nicovaras22@gmail.com>
2015-10-26 23:25:01 -0300

    Retrieve mp3 file list
    Scans recursively for mp3 files in a given folder. Plays the first one.

--- src/player/core.clj
+++ src/player/core.clj
@@ -1,8 +1,23 @@
 (ns player.core
+  (:import java.io.File)
   (:gen-class))
 (use 'clj-audio.core)
+
+(defn mp3-paths [d]
+  (filter
+    (fn [x] (re-matches #".*mp3" (str x)))
+    (reduce
+     (fn [x f]
+       (if (.isDirectory f)
+         (concat x (mp3-paths f))
+         (concat x [(.getCanonicalPath f)])))
+     []
+     (.listFiles d))))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (-> (java.io.File. ".") .getAbsolutePath))
-  (-> (->stream "a.mp3") decode play))
+  (let [first-mp3 ( first (mp3-paths (File. ".")))]
+    (println first-mp3)
+    (-> (->stream first-mp3) decode play)
+  ))
